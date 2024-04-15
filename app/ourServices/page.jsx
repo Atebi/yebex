53acb2f3175ff38e02378576126aa275bfba71c8
kingsleyibe <ibekingsley66@gmail.com>
2024-04-15 15:40:36 +0100

    Initial clone commit

--- app/ourServices/page.jsx
+++ app/ourServices/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ServicesPage = () => {
   return (
     <div className="mt-[72px] py-8 text-white lg:py-16">
-      This is the service page
+      I am working on this Services Page
     </div>
   );
 };
