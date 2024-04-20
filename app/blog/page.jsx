184412cbf2e17436499a24440db55b30c9d1e9e2
lawrence <chylau12@hotmail.com>
2024-04-20 15:49:25 +0100

    io

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -2,8 +2,104 @@ import React from "react";
 
 const BlogPage = () => {
   return (
-    <div className="mt-[72px] py-8 text-white lg:py-16">
-      This is the blog page
+    <div className="mt-[72px] py-8 lg:py-16">
+      <div class="min-h-sceen mx-auto max-w-screen-xl px-5">
+        <div class="flex flex-col items-center">
+          <h2 class="mt-5 text-5xl font-bold tracking-tight text-lime-500">
+            FAQ
+          </h2>
+          <p class="mt-3 text-xl text-white">Frequenty asked questions</p>
+        </div>
+        <div class="mx-auto mt-8 grid max-w-xl divide-y divide-neutral-200">
+          <div class="py-5">
+            <details class="group">
+              <summary class="flex cursor-pointer list-none items-center justify-between font-medium">
+                <span class="text-3xl text-lime-500">
+                  {" "}
+                 Question 1?
+                </span>
+                <span class="text-2xl text-lime-500 transition group-open:rotate-180">
+                  <svg
+                    fill="none"
+                    height="24"
+                    shape-rendering="geometricPrecision"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="1.5"
+                    viewBox="0 0 24 24"
+                    width="24"
+                  >
+                    <path d="M6 9l6 6 6-6"></path>
+                  </svg>
+                </span>
+              </summary>
+              <p class="group-open:animate-fadeIn mt-3 text-white">
+                SAAS platform is a cloud-based software service that allows
+                users to access and use a variety of tools and functionality.
+              </p>
+            </details>
+          </div>
+          <div class="py-5">
+            <details class="group">
+              <summary class="flex cursor-pointer list-none items-center justify-between font-medium">
+                <span class="text-3xl text-lime-500">
+                  {" "}
+                  Question 2?
+                </span>
+                <span class="text-2xl text-lime-500 transition group-open:rotate-180">
+                  <svg
+                    fill="none"
+                    height="24"
+                    shape-rendering="geometricPrecision"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="1.5"
+                    viewBox="0 0 24 24"
+                    width="24"
+                  >
+                    <path d="M6 9l6 6 6-6"></path>
+                  </svg>
+                </span>
+              </summary>
+              <p class="group-open:animate-fadeIn mt-3 text-white">
+                SAAS platform is a cloud-based software service that allows
+                users to access and use a variety of tools and functionality.
+              </p>
+            </details>
+          </div>
+          <div class="py-5">
+            <details class="group">
+              <summary class="flex cursor-pointer list-none items-center justify-between font-medium">
+                <span class="text-3xl text-lime-500">
+                  {" "}
+                  Question 3?
+                </span>
+                <span class="text-2xl text-lime-500 transition group-open:rotate-180">
+                  <svg
+                    fill="none"
+                    height="24"
+                    shape-rendering="geometricPrecision"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="1.5"
+                    viewBox="0 0 24 24"
+                    width="24"
+                  >
+                    <path d="M6 9l6 6 6-6"></path>
+                  </svg>
+                </span>
+              </summary>
+              <p class="group-open:animate-fadeIn mt-3 text-white">
+                SAAS platform is a cloud-based software service that allows
+                users to access and use a variety of tools and functionality.
+              </p>
+            </details>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
